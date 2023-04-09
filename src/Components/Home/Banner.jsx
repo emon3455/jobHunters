569dae60641553314c508ba844a801949962c087
Emon <emon.mhk69@gmail.com>
2023-04-10 00:23:59 +0600

    home banner done

---
+++ src/Components/Home/Banner.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="banner-container">
+            <div className="md:w-2/3 space-y-4">
+                <h2 className='text-7xl font-semibold'>
+                    Get One Step Closer To Your <span className='text-violet-600'>Dream Job</span>
+                </h2>
+
+                <p className='text-gray-500'>
+                Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.
+                </p>
+
+                <button className='btn-primary'>Get Started</button>
+
+            </div>
+            <div className="banner-img">
+                <img className='w-full' src="https://github.com/emon3455/demo-picture/blob/main/assets/All%20Images/P3OLGJ1%20copy%201.png?raw=true" alt="" />
+            </div>     
+        </div>
+    );
+};
+
+export default Banner;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <main>
-            This is home page.
+            <Banner></Banner>
         </main>
     );
 };
