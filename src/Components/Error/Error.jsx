4ba2be7c7459ca25a3bcc4c4bf6423fb9e08882b
Emon <emon.mhk69@gmail.com>
2023-04-11 20:07:42 +0600

    error page with erro page fully done

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import Lottie from "lottie-react";
-import error from "../../assets/error.json"
+import errors from "../../assets/error.json"
+import { Link, useRouteError } from 'react-router-dom';
 
 const Error = () => {
+
+    const { error, status } = useRouteError()
+
+    // return (
+    //     <div className='h-screen flex flex-col justify-center items-center'>
+    //         <div className="h-3/4">
+    //             <Lottie className='w-full h-full' animationData={errors} loop={true}></Lottie>
+    //         </div>
+    //     </div>
+    // );
+
     return (
-        <div className='h-screen flex flex-col justify-center items-center'>
-            <div className="h-3/4">
-                <Lottie className='w-full h-full' animationData={error} loop={true}></Lottie>
+        <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
+            <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+                <div className='max-w-md text-center'>
+                    <h2 className='mb-8 font-extrabold text-9xl text-gray-600'>
+                        <Lottie className='w-full h-full' animationData={errors} loop={true}></Lottie>
+                        <span className='sr-only'>Error</span> {status || ""}
+                    </h2>
+                    <p className='text-2xl font-semibold md:text-3xl mb-8'>
+                        {error?.message}
+                    </p>
+                    <Link to='/' className='px-8 py-3 font-semibold rounded-lg bg-sky-300 text-white'>
+                        Back to homepage
+                    </Link>
+                </div>
             </div>
-        </div>
+      </section>
     );
+    
 };
 
 export default Error;
